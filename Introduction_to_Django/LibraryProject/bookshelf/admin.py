dc0679da4233b0a0b278940e99e2e08ad238d72b
Paul Promise <paul.promise@outlook.com>
2025-11-01 03:05:24 +0000

    registered book model in the admin.py file

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Book
 
 # Register your models here.
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'published_year')
+    search_fields = ('title', 'author')
+    list_filter = ('published_year',)
+    ordering = ('published_year',)
+    
+    
