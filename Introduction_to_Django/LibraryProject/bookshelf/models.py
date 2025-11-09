f906198ad5d8aabb1b174c0a5b2795619c819f28
Paul Promise <paul.promise@outlook.com>
2025-11-09 13:37:28 +0000

    commit Update models.py

--- Introduction_to_Django/LibraryProject/bookshelf/models.py
+++ Introduction_to_Django/LibraryProject/bookshelf/models.py
@@ -6,4 +6,4 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=200, blank=False)
     author = models.CharField(max_length=100, blank=False)
-    published_year = models.IntegerField()
+    publication_year = models.IntegerField()
