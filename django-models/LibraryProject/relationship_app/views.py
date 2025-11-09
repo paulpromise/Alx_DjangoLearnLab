425b61b2b53503142df820baf7b5aa9bccc630b5
Paul Promise <paul.promise@outlook.com>
2025-11-09 14:58:36 +0000

    importing generic view library

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.views.generic import DetailView
+from django.views.generic.detail import DetailView
 from . import Book
 from .models import Library
 
